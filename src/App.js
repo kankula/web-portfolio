2c602c405fafca881b6a8a0602b78acf5c5f5582
kankula <kiri0944@gmail.com>
2021-12-05 03:02:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,23 @@ import Home from './contents/Home';
 import Skills from './contents/Skills';
 import About from './contents/About';
 import Projects from './contents/Projects';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, HashRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <Router basename={"/web-portfolio"}>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/skills" element={<Skills />} />
-          <Route exact path="/projects" element={<Projects />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-        <Background />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/skills" element={<Skills />} />
+            <Route exact path="/projects" element={<Projects />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+          <Background />
 
-      </div>
-    </Router>
-
+        </div>
+      </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode basename={"/web-portfolio"}>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
