96ebcbc6961307d98781f55168f4db6b7a8c40b7
kankula <kiri0944@gmail.com>
2021-12-09 01:32:09 +0700

    add project

--- src/contents/Projects.js
+++ src/contents/Projects.js
@@ -3,6 +3,7 @@ import './Projects.css'
 import Proj_pic1 from '../img/p1.png'
 import Proj_pic2 from '../img/p2.png'
 import Proj_pic3 from '../img/p3.png'
+import Proj_pic4 from '../img/p4.png'
 
 const Projects = () => {
     return (
@@ -36,6 +37,15 @@ const Projects = () => {
                             <div className="face face2">
                                 <a href="https://kankula.github.io/App-Shop/" target="_blank">Open Web</a>
                                 <a href="https://github.com/kankula/App-Shop.git" target="_blank">Open Code</a>
+                            </div>
+                        </div>
+                        <div className="project-item">
+                            <div className="face face1">
+                                <img src={Proj_pic4} alt="p4.png" className="Proj_pic4" />
+                            </div>
+                            <div className="face face2">
+                                <a href="https://kankula.github.io/Test-Post/" target="_blank">Open Web</a>
+                                <a href="https://github.com/kankula/Test-Post.git" target="_blank">Open Code</a>
                             </div>
                         </div>
                     </div>
